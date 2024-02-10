39b692a418bc0f523478c6b5905d2ab5b88e1fc3
besar77 <bd48380@ubt-uni.net>
2024-02-10 13:03:57 +0100

    Created model task

---
+++ vue/src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state: {
+    besar: "Hello nga besi",
+  },
+  getters: {},
+  actions: {},
+  mutations: {},
+  modules: {},
+});
+
+export default store;
---
+++ vue/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 className="text-3xl font-bold underline">{{ test }}</h1>
+  </div>
+</template>
+
+<script setup>
+import store from "@/store";
+import { computed } from "vue";
+
+const test = computed(() => store.state.besar);
+</script>
+
+<style lang="scss" scoped></style>
